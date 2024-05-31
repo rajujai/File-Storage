6d49959128b2bee09c565a393f0cd97db88e555d
rajujai <rajujaijangra@gmail.com>
2024-05-31 10:12:12 +0530

    ini setup

---
+++ src/main/java/com/udss/UdssApplication.java
@@ -0,0 +1,13 @@
+package com.udss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udss/udss/UdssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udss.udss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
